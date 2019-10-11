ff4824b3346e0dcc558607b138ded1acfc64a6c4
Rohit Mondal <pisces.rohit99@gmail.com>
2019-10-11 23:52:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyCAbJPCMSlQZO_YUhywG65cEIRgLcf4jOM",
-  authDomain: "bookmyshow-2513d.firebaseapp.com",
-  databaseURL: "https://bookmyshow-2513d.firebaseio.com",
-  projectId: "bookmyshow-2513d",
+  apiKey: "YOUR KEY",
+  authDomain: "YOUR DOMAIN",
+  databaseURL: "YOUR DATABASE URL",
+  projectId: "YOUR PROJECT ID",
   storageBucket: "",
-  messagingSenderId: "933733697512",
-  appId: "1:933733697512:web:2f15deb848ed0b2d89065d",
-  measurementId: "G-NSQLTQTHN8"
+  messagingSenderId: "YOUR SENDER ID",
+  appId: "YOUR APP ID",
+  measurementId: "YOUR MEASUREMENT ID"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
